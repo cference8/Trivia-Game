706632d501f5a1c0fd4612baa25d4b297beeb3f7
Chris <cference8@gmail.com>
2024-01-20 10:25:51 -0600

    initial commit

---
+++ TriviaHomework-FinalDraft.hyperesources/triviaInfo.js
@@ -0,0 +1,79 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"MINNESOTA GEOGRAPHY TRIVIA";
+	window.questions = [		"The Mayo Clinic is in which city in Minnesota?", 
+								"Who created the world's largest twine ball, which is in Darwin?",
+								"The first intercollegiate basketball game was played in Minnesota during what year?", 
+								"The first official hit in the Metrodome was made by Pete Rose playing in a preseason game for what team?", 
+								"Minnesota's waters flow outward in how many directions?",
+								"Frank C. Mars of Minnesota introduced what candy bar in 1923?",
+								"Minnesota students invented in-line roller skates in what year?",
+								"Which of these were not invented in Minnesota?",
+								"The Chanhassen Dinner Theater in Minnesota is known for what?",
+								"Many of Minnesota lakes have the same name. Which of these is one of the most common names for a Minnesota lake?" ];
+	window.correctAnswer = [ 	"A", "A", "C", "A", "C", "C", "C", "A", "B", "A" ]; 
+	window.answerA = [ 			"ROCHESTER", 
+								"FRANCIS A. JOHNSON", 
+								"1885", 
+								"PHILADELPHIA PHILLIES", 
+								"FIVE",
+								"SNICKERS",
+								"1979",
+								"GOLF CLUB",
+								"OLDEST CONTINUOUSLY RUNNING THEATER IN THE COUNTRY",
+								"MOUNTAIN LAKE" ];
+	window.answerB = [ 			"MADISON", 
+								"RALPH W. SAMUELSON", 
+								"1890", 
+								"BOSTON RED SOX",
+								"FOUR",
+								"CRUNCH",
+								"1981",
+								"STAPER",
+								"LARGEST DINNER THEATER IN THE COUNTRY",
+								"WASHINGTON LAKE" ];
+	window.answerC = [ 			"BLOOMINGTON",
+								"BRENNAN OLSON",
+								"1895", 
+								"NEW YORK YANKEES", 
+								"THREE",
+								"MILKY WAY",
+								"1980",
+								"GREEN GIANT VEGATABLES",
+								"LARGEST REGIONAL PLAYHOUSE IN THE COUNTRY",
+								"VERONICA LAKE" ];
+	window.answerD = [ 			"MINNEAPOLIS",  
+								"MILBURN HENKE",
+								"1900", 
+								"CHICAGO WHITE SOX",
+								"TWO",
+								"THREE MUSKETEERS",
+								"1982",
+								"AVEDA BEAUTY PRODUCTS",
+								"LARGEST URBAN SCULPTURE GARDEN IN THE COUNTRY",
+								"LONG LAKE" ];
+	window.answerE = [ 			"ST. CLOUD", 
+								"CHARLES DARWIN", 
+								"1915", 
+								"LOS ANGELES DODGERS", 
+								"ONE",
+								"100 GRAND BAR",
+								"1983",
+								"SPAM",
+								"SMALLEST DINNER THEATER IN THE COUNTRY",
+								"BIG LAKE" ];
+	window.response = [ 		"This clinic is known world wide for its newest methods of treatments.", 
+								"This twine ball weighs 17,400 pounds and is twelve feet in diameter.'", 
+								"The exact date was February 9,1895.", 
+								"This was during a preseason game.", 
+								"These ways are: north to Hudson Bay, east to the Atlantic Ocean, and south to the Gulf of Mexico.",
+								"He introduced the Snickers bar in 1930 and the Three Musketeers bar in 1937. He didn't have anything to do with the Crunch bar.",
+								"These students were Scott Olson and Brennan Olson. They were looking for a way to practice hockey in the off season.",
+								"Some other inventions in Minnesota were Bisquick, HMOs, and Masking and Scotch tape.",
+								"The Guthrie Theater is largest regional playhouse in the country, the Old Log Theater is theoldest continuously running theater in the country, and the Minneapolis Sculpture Garden is the largest urban sculpture garden in the country. All of these are in Minnesota.",
+								"Of the more than 11,000 lakes in Minnesota, Long, Mud, Rice, Bass, Round, Horseshoe, Twin and Island are the most common names." ];
+							
+}
+						
